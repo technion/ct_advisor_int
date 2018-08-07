cddaedbfb7287d09e91caaf9781525a35f190326
Technion <technion@lolware.net>
2018-08-07 05:06:27 +0000

    Cleanup route names and paths. Address CSRF token issue on unsubscribe route.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-  config.host = "http://lolware.net:3000"
+  config.host = "http://lolware.net:4567"
 
   # Do not eager load code on boot.
   config.eager_load = false
