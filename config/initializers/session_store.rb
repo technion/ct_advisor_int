78bf5193687c11e77316ed411f13b55e7dfb3a38
Technion <technion@lolware.net>
2022-02-16 00:33:06 +0000

    mprove security headesr

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,2 @@
 # Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_ct_advisor_int_session'
+Rails.application.config.session_store :cookie_store, key: '_ct_advisor_int_session', secure: Rails.env.production?
